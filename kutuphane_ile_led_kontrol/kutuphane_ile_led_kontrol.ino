b70b0a634fb04f4eb7211b67972dbb735e84cacf
ugur05 <f.ugurkececi@gmail.com>
2015-10-15 18:01:01 +0300

    ARDUİNO İÇİN KENDİ KÜTÜPHANEMİZİ OLUŞTURMA
    Projelerimizde , ürettiğmiz prototip ürünlerde , gerek ucuzluğuyla
    gerekse yazılım kolaylığıyla vazgecilmezimiz olan arduino için bu
    yazımızda kendi kütüphanemezi yazacağız.
    
    Evet arduino kullanan arkadaşlar çok iyi bilirler ki , kütüphane arduino
    için vaz geçilmez bir unsurdur.Sebebi ise kullanım kolaylığı
    sağlamasıdır

---
+++ kutuphane_ile_led_kontrol/kutuphane_ile_led_kontrol.ino
@@ -0,0 +1,19 @@
+#include <led.h>
+
+led gonder;
+void setup() {
+
+}
+
+void loop() {
+  ////***** 500ms lik manuel olarak yapılan flip flop *****///////
+  gonder.led_yak();
+  delay(500);
+  gonder.led_sondur();
+  delay(500);
+  
+  ////***** 2000ms lik kütüphaneden kullanarak yapılan flip flop *****///////
+  gonder.led_flipflop(2000);
+  
+  gonder.led_flipflop(2000);
+}
